407cf779a4d87a67d550f60a6bd9fad4ec2ea61b
yhbae106 <yhbae106@gmail.com>
2026-08-19 16:45:13 +0900

    add shared action sync configuration

---
+++ action-sync-config.js
@@ -0,0 +1,20 @@
+window.DCM_ACTION_SYNC={
+  endpoint:'',
+  spreadsheetId:'1LbEuintnEZbnwJXZcrbTWZNECWMg4ry7_UwRsQ9end0',
+  sheetName:'Action Board',
+  editors:['배영훈','정직한','임인숙'],
+  pollMs:60000
+};
+
+window.DCM_ACTION_REASONS={
+  '01':'시스템 미구축',
+  '02':'거래처 사용거부',
+  '03':'ERP Interface 오류',
+  '04':'Master 불일치',
+  '05':'신규거래처',
+  '06':'공급중단 예정',
+  '07':'사용법 미숙',
+  '08':'데이터 오류',
+  '09':'확인 중',
+  '99':'기타'
+};
